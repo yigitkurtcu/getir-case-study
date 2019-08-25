6b2a0515c68535a02df4e5182f7b2aa9a0269243
Yiğit Kurtçu <yiqit-35@hotmail.com>
2019-08-25 19:13:35 +0300

    testing uri for heroku mongodb error

--- db.js
+++ db.js
@@ -3,14 +3,21 @@ const mongoose = require('mongoose');
 module.exports = {
   mongoose,
   connect: () => {
+    console.log(process.env.MONGO_URI);
     mongoose.Promise = Promise;
-    mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      auto_reconnect: true
-    });
+    mongoose
+      .connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        auto_reconnect: true
+      })
+      .catch(err => {
+        console.log(err);
+      });
   },
   disconnect: done => {
-    mongoose.disconnect(done);
+    mongoose.disconnect(done).catch(err => {
+      console.log(err);
+    });
   }
 };
 
